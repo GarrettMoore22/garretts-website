2a3e54b73506bb4790ba62a156e6dcc94a458d88
GarrettMoore22 <garrett22smoore@gmail.com>
2021-03-15 13:14:10 -0700

    changed links to allow for hash router

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import './App.css';
 import NavMenu from './components/Nav.js'
 import "bootstrap/dist/css/bootstrap.min.css";
 import AboutMe from './components/AboutMe.js'
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className="App">
         <NavMenu />
         <Switch>
                     <Route path="/about-me" component={AboutMe} />
         </Switch>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,9 +10,9 @@ function NavMenu() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/about-me">About Me</Nav.Link>
-                    <Nav.Link href="/resume">Resume</Nav.Link>
-                    <Nav.Link href="/previous-projects">Previous Projects</Nav.Link>
+                    <Nav.Link href="/#/about-me">About Me</Nav.Link>
+                    <Nav.Link href="/#/resume">Resume</Nav.Link>
+                    <Nav.Link href="/#/previous-projects">Previous Projects</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
