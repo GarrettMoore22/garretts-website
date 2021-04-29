c793ed03ae5ed4e01fd65d60602b4358d50f3cae
GarrettMoore22 <garrett22smoore@gmail.com>
2021-04-29 14:47:39 -0700

    updated projects page with previous work, updated styling on entire site

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,19 +8,39 @@ function Projects() {
         <div className="projects">
             <div className="projects-container">
                 <div className="project-container">
-                    <h2 className="project-title">Space Age</h2>
-                    <p className="project-description">
-                        <img src={SnowPass} className="project-image" alt="SnowPass"></img>
-                        Manage and update four websites for student run publications through the use of HTML, CSS, PHP, 
-                        and Javascript languages and the Wordpress platform.
-                    </p>
-                </div>
-                <div className="project-container">
                     <h2 className="project-title">SnowPass</h2>
                     <p className="project-description">
-                        <img src={SpaceAge} className="project-image" alt="Spaceage"></img>
-                        Tutor students in previously completed courses, largely in the Computer Science department.
+                        A web application made in React for my Senior Project at the University of Nevada, 
+                        Reno in 2021. I primarily designed and implemented the UI, routing, and webcam display, 
+                        as well as assisting in styling various components across the site. The goal of the 
+                        project is to better inform drivers about road conditions, and to make driving on 
+                        winter roads in Nevada and California safer. It does this by providing a user-friendly 
+                        interface that displays live footage and weather information for locations along a user’s 
+                        chosen path.
                     </p>
+                    <a className="github-link" href="https://thesnowpass.com">
+                        <p className="github-link">SnowPass</p>
+                    </a>
+                    <img src={SnowPass} className="project-image" alt="SnowPass"></img>
+                </div>
+                <div className="project-container">
+                    <h2 className="project-title">Space Age</h2>
+                    <p className="project-description">
+                        A game made as an entry in the 2019 Biggest Little Hackathon for UNR’s ACM chapter, 
+                        created with C# in Unity. I created, edited, and formatted all of the graphics and 
+                        audio for the project, including writing original music. The goal of the game was 
+                        to work together with another person to survive as long as possible flying around 
+                        space. One person would pilot the ship, while the other player shot at enemy ships 
+                        as well as repaired the ship’s systems. If any system took too much damage, the system 
+                        would go offline until it was repaired. 
+                    </p>
+                    <a className="github-link" href="https://github.com/kylebrain/space-age">
+                        <p className="github-link">Github Repository</p>
+                    </a>
+                    <a href="https://github.com/kylebrain/space-age/releases">
+                        <p className="github-link">Download</p>
+                    </a>
+                    <img src={SpaceAge} className="project-image" alt="Spaceage"></img>
                 </div>
             </div>
         </div>
